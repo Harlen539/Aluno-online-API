671581bcf61c54b18b4ba37c5a90d00feb6b52bc
Harlen Henrick <harlenhenrick14@gmail.com>
2026-04-07 00:15:57 -0300

    Delete Aluno-online-Api-Java-main directory

--- Aluno-online-Api-Java-main/src/main/java/br/com/alunoonline/api/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.alunoonline.api.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- Aluno-online-Api-Java-main/src/main/java/br/com/alunoonline/api/repositories/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alunoonline.api.repositories;
-
-import br.com.alunoonline.api.models.Professor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProfessorRepository extends CrudRepository<Professor, Long>  {
-}

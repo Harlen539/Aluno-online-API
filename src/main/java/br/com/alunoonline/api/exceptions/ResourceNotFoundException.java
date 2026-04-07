c26fd4487a9e4120e72f243b4dddfe9ef6974cc4
Harlen Henrick <harlenhenrick14@gmail.com>
2026-04-07 00:20:10 -0300

    Add API project and fix configuration warnings

---
+++ src/main/java/br/com/alunoonline/api/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.alunoonline.api.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
